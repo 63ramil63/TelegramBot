4893f5b76f2596717dd58603e563628009a39e24
63ramil63 <63ramil63@gmail.com>
2025-04-02 10:15:34 +0400

    Фикс ошибки с получением файла конфига

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,6 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.sql.SQLException;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main{
@@ -14,8 +12,8 @@ public class Main{
     public static String propertyPath = "";
 
     public static void main(String[] args) throws TelegramApiException {
+        propertyPath = args[0];
         TelegramBotsApi tgBot = new TelegramBotsApi(DefaultBotSession.class);
         tgBot.registerBot(new TelegramBot());
-        propertyPath = args[0];
     }
 }
